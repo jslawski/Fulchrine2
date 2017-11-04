2a433df9298e2e4ba5dee02bf4e2d328d4201187
jslawski <jslawski@umich.edu>
2017-11-04 16:19:41 -0700

    InControl integration and basic movement

---
+++ Assets/PlayerCharacter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class PlayerCharacter : MonoBehaviour {
+
+	protected InputDevice device;
+
+	float moveSpeed = 5f;
+
+	void Awake()
+	{
+		this.device = InputManager.Devices[0];
+	}
+
+	// Use this for initialization
+	void Start() {
+		
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (this.device.LeftStick.Vector.magnitude > 0)
+		{
+			this.Move(this.device.LeftStick.Vector);
+		}
+	}
+
+	void Move(Vector2 moveVector)
+	{	
+		Vector3 moveDirection = new Vector3(moveVector.x, 0, moveVector.y);
+		this.gameObject.transform.Translate(moveDirection * Time.deltaTime * this.moveSpeed);
+	}
+}
